d0e20bfbaa488f385b55b21ed6a048c864f6cae9
Jesie Oldenburg <kitglo46@gmail.com>
2018-06-15 12:02:07 -0500

    adding slider component labels and styles; initial fb setup

---
+++ retrack/src/fb_config/fb_init.js
@@ -0,0 +1,18 @@
+import Rebase from 're-base'
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyASjpPI_5TxmB_PyOW9zAXaTDQJY8EvYXY",
+    authDomain: "retrack-2c47a.firebaseapp.com",
+    databaseURL: "https://retrack-2c47a.firebaseio.com",
+    projectId: "retrack-2c47a",
+    storageBucket: "retrack-2c47a.appspot.com",
+    messagingSenderId: "660367147167"
+  };
+
+firebase.initializeApp(config);
+
+const app = firebase.initializeApp(config)
+const base = Rebase.createClass(app.database())
+
+export { base }
