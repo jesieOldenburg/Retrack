af5431a5c212c071964205654e4cedb03e8d810d
Jesie Oldenburg <kitglo46@gmail.com>
2018-06-19 17:37:15 -0500

    user tracks printing to cards; working post to firebase for track editing

--- retrack/src/components/UserTracks.js
+++ retrack/src/components/UserTracks.js
@@ -6,9 +6,6 @@ import Header from './Header';
 import './UserTracks.css';
 import base from './fb_init';
 
-let trackData;
-let db_tracks;
-
 class UserTracks extends React.Component {
     constructor (props) {
         super(props);
@@ -18,6 +15,7 @@ class UserTracks extends React.Component {
           items: []
         };
         this.handleEdit = this.handleEdit.bind(this);
+        this.deleteTrack = this.deleteTrack.bind(this);
     }
 
     componentDidMount () {
@@ -25,38 +23,72 @@ class UserTracks extends React.Component {
         context: this,
         asArray: true,
         then(data){
+          console.log("This is your data", data);
             this.setState({
                 isLoaded: true,
-                items: data.items,
+                items: data,
             });
-          console.log("This is your data", data);
+
         },
 
       });
      }//Component Did Mount Closing Bracket
 
-    handleEdit = (e) => {
-        
+    handleEdit = (item) => {
+
+      base.post(`tracks/${item.key}`, {
+        data: {
+            track_title: `${item.track_title}`,
+            time_signature: `${item.time_signature}`,
+            bpm: `${item.bpm}`,
+            notes: `${item.notes}` 
+
+        },
+        then(err){
+            console.log("erroro");
+        }
+      });
+
+    }
+
+    deleteTrack = (e) => {
+
     }
 
     render () {
-        const { error, isLoaded, items } = this.state;
+        console.log("stateee", this.state);
+        if (this.state.isLoaded === true) {
+            const db_tracks  = this.state.items.map((item, index ) => {
+                return <Card key={item.key} className="song-cards">
+                <CardBody>
+                  <CardTitle name={item.key} className="card-title">{item.track_title}</CardTitle>
+                  <CardText className="card-text">{item.time_signature}</CardText>
+                  <CardText className="card-text">{item.bpm}</CardText>
+                  <CardText className="card-text">{item.notes}</CardText>
+                  <Button onClick={() => { this.handleEdit(item) }}>Edit Track</Button>
+                  <Button onClick={this.deleteTrack}>Delete Track</Button>
+                </CardBody>
+              </Card>
+          })
         return (
         <div>
             <Header name="Your Saved Tracks" />
-            <div>
-            {items.map((index, item) => {
-               <p>{item.bpm}</p> 
-            })}
+            <div className="card-container">
+                {db_tracks}
             </div>
         </div>
-    );
+        
+        );
+
+        } else {
+            return <div>LOOOADD.....</div>
+        }
             
         }
 
 
 
-    }
+    } 
 
 
 export default UserTracks;
